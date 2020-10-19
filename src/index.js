3f04d765919ac0f1ffdc86c0786803371b613309
Fernando Estevez <fernandoestevez8494@gmail.com>
2020-10-18 21:58:04 -0400

    trim value from productCode field

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const openBtn = document.getElementById('open-button')
 openBtn.addEventListener('click', function(event) {
     var productCode = document.getElementById('pcInput').value
     if (productCode != "") {
-        if (!openPcWindow(productCode)) {
+        if (!openPcWindow(productCode.trim())) {
             ipcRenderer.send('pc-not-found', true)
         }
     } else {
