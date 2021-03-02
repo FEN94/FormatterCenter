5e017b4766c13187c20ba250e2cc498035833f6f
Fernando Estevez <fernandoestevez8494@gmail.com>
2021-03-01 23:38:29 -0400

    Clean ProductCode textInput once folder is created

--- src/index.js
+++ src/index.js
@@ -35,26 +35,6 @@ addBtn.addEventListener('click', () => {
         ipcRenderer.send('empty', "Select printing type")
     } else {
         fillTable(productCode, subProgram, printingType)
-        // var newPcTable = document.getElementById('newPcTable')
-        // var tbody = newPcTable.children[1]
-        // var row = document.createElement('tr')
-        // var tdProductCode = document.createElement('td')
-        // var tdProgram = document.createElement('td')
-        // var tdSubProgram = document.createElement('td')
-        // var tdPrintingType = document.createElement('td')
-        // tdProductCode.innerHTML = productCode.toUpperCase().trim()
-        // tdProgram.innerHTML = productCode.substring(0, 2).toUpperCase().trim()
-        // tdPrintingType.innerHTML = printingType
-        // if (subProgram) {
-        //     tdSubProgram.innerHTML = productCode.substring(2, 4).toUpperCase().trim()
-        // } else {
-        //     tdSubProgram.innerHTML = "None"
-        // }
-        // row.appendChild(tdProductCode)
-        // row.appendChild(tdProgram)
-        // row.appendChild(tdSubProgram)
-        // row.appendChild(tdPrintingType)
-        // tbody.appendChild(row)
     }
 })
 
@@ -77,6 +57,7 @@ createBtn.addEventListener('click', () => {
         while (tbody.firstChild) {
             tbody.removeChild(tbody.lastChild)
         }
+        document.getElementById('newPc').value = ""
         populateList()
         ipcRenderer.send('success', "Folder(s) created successfully")
     }
